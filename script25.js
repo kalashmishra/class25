52781706738827a8d11ebb26dc7596a252ede1db
Kalash Mishra <mishrakalash18@gmail.com>
2023-10-16 15:02:27 +0530

    change

--- script25.js
+++ script25.js
@@ -27,18 +27,18 @@ function imgSlider(val){
 
 document.querySelector("#one").addEventListener("click",function(){
   
-    imgSlider(`/img/pepsi001.png`);
+    imgSlider(`./pepsi001.png`);
     document.querySelector("#main").style.backgroundColor="#0062be";
    
 
 })
 document.querySelector("#two").addEventListener("click",function(){
-  imgSlider(`/img/pepsi002.png`);
+  imgSlider(`./pepsi002.png`);
   document.querySelector("#main").style.backgroundColor="#e60c2c";
       
 })
 document.querySelector("#three").addEventListener("click",function(){
-      imgSlider(`/img/pepsi003.png`);
+      imgSlider(`./pepsi003.png`);
       document.querySelector("#main").style.backgroundColor="#1e1e1e";
  
     
